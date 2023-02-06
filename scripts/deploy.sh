395a7e8fd2006f81a8156050c4c08a3043517f33
hunjeong93 <rhkrgnswjd@naver.com>
2023-02-06 16:33:11 +0900

    deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # 빌드 파일의 이름이 콘텐츠와 다르다면 다음 줄의 .jar 파일 이름을 수정하시기 바랍니다.
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
+BUILD_JAR=$(ls /home/hunjeong/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
+echo "> 현재 시간: $(date)" >> /home/hunjeong/action/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/hunjeong/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+echo "> build 파일 복사" >> /home/hunjeong/action/deploy.log
+DEPLOY_PATH=/home/hunjeong/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/hunjeong/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/hunjeong/action/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
+  echo "> kill -9 $CURRENT_PID" >> /home/hunjeong/action/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
